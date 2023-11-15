7e86c92962f4a73f69e89cc9d03711afc1060619
sarasri04 <srithar4763@gmail.com>
2023-11-15 15:02:54 +1100

    homepage changes

--- src/components/course/course.js
+++ src/components/course/course.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import '../register/sytle.css';
 
 const Course = () => (
     <div className="form-body">
 
       Course List
   
+      <li>Java</li>
+      <li>ReactJS</li>
+      <li>Angular</li>
+      <li>Java Interview Questions</li>
+      <li>Spring Boot</li>
+      <li>MicroServices</li>
+      <li>git/Jira</li>
+      <li>Jenkins</li>
+      <li>Java</li>
+      <li>ReactJS</li>
+      <li>Angular</li>
+      <li>Java Interview Questions</li>
+      <li>Spring Boot</li>
+      <li>MicroServices</li>
+      <li>git/Jira</li>
+      <li>Jenkins</li>
       <li>Java</li>
       <li>ReactJS</li>
       <li>Angular</li>
